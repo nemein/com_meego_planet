bb5233f8860ef4521bfb8e6d96dee358c7c95a1b
Henri Bergius <henri.bergius@iki.fi>
2011-03-16 12:47:21 +0200

    Item listings

---
+++ controllers/latest.php
@@ -0,0 +1,39 @@
+<?php
+class com_meego_planet_controllers_latest
+{
+    public function __construct(midgardmvc_core_request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function get_items(array $args)
+    {
+        $this->data['title'] = 'Latest blogs';
+        
+        // Query for latest items
+        $q = new midgard_query_select
+        (
+            new midgard_query_storage('com_meego_planet_item')
+        );
+        $q->add_order(new midgard_query_property('metadata.published'), SORT_DESC);
+        
+        // Handle paging
+        $this->check_page($args, $q);
+        
+        $q->execute();
+        
+        $this->data['items'] = array_map
+        (
+            function($item)
+            {
+                // TODO: Get author and avatar
+                return $item;
+            },
+            $q->list_objects()
+        );
+    }
+    
+    private function check_page(array $args, midgard_query_select $q)
+    {
+    }
+}
---
+++ controllers/top.php
@@ -0,0 +1,32 @@
+<?php
+class com_meego_planet_controllers_top
+{
+    public function __construct(midgardmvc_core_request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function get_items(array $args)
+    {
+        $this->data['title'] = 'Top items';
+        
+        // FIXME: Query for top items instead
+        $q = new midgard_query_select
+        (
+            new midgard_query_storage('com_meego_planet_item')
+        );
+        $q->add_order(new midgard_query_property('metadata.score'), SORT_DESC);
+
+        $q->execute();
+        
+        $this->data['items'] = array_map
+        (
+            function($item)
+            {
+                // TODO: Get author and avatar
+                return $item;
+            },
+            $q->list_objects()
+        );
+    }
+}
