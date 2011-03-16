a5b779796350a73ccc62b9887ea49a90630a04b0
Henri Bergius <henri.bergius@iki.fi>
2011-03-16 11:48:25 +0200

    Typo fix

--- injector.php
+++ injector.php
@@ -7,7 +7,7 @@ class com_meego_planet_injector
     public function inject_template(midgardmvc_core_request $request)
     {
         // Ensure our elements are available also for other components
-        $request->add_component_to_chain($this->mvc->component->get('com_meego_planet'));
+        $request->add_component_to_chain(midgardmvc_core::get_instance()->component->get('com_meego_planet'));
 
         // Replace the default MeeGo sidebar with our own
         $route = $request->get_route();
