3f9b277e787213fa097a981f4b9265cbef93f593
Henri Bergius <henri.bergius@iki.fi>
2011-03-17 15:45:12 +0200

    Add outbound feeds

--- controllers/top.php
+++ controllers/top.php
@@ -24,5 +24,54 @@ class com_meego_planet_controllers_top
                 }
             )
         );
+
+        midgardmvc_core::get_instance()->head->add_link
+        (
+            array
+            (
+                'rel' => 'alternate',
+                'type' => 'application/rss+xml',
+                'title' => $this->data['title'],
+                'href' => midgardmvc_core::get_instance()->dispatcher->generate_url('index_rss', array(), $this->request)
+            )
+        );
+    }
+    
+    public function get_feed(array $args)
+    {
+        // Read items from Content Repository
+        $this->get_items($args);
+        
+        // Set up feed
+        midgardmvc_core::get_instance()->component->load_library('Feed');
+        $feed = new ezcFeed();
+        $feed->title = $this->data['title'];
+        $feed->description = '';
+        
+        $now = new DateTime();
+        $feed->published = $now->format(DateTime::RSS);
+        
+        $link = $feed->add('link');
+        $link->href = midgardmvc_core::get_instance()->dispatcher->generate_url('index', array(), $this->request);
+        
+        array_walk
+        (
+            $this->data['items'],
+            function($item) use ($feed)
+            {
+                $feeditem = $feed->add('item');
+                $feeditem->title = $item->title;
+                $feeditem->description = $item->content;
+                $feeditem->published = $item->published->format(DateTime::RSS);
+                
+                $author = $feeditem->add('author');
+                $author->name = $item->firstname;
+                
+                $link = $feeditem->add('link');
+                $link->href = $item->url;
+            }
+        );
+        
+        $this->data['feed'] = $feed;
     }
 }
