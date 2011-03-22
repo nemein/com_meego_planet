23d8114f7330938517361b09242bcac695c0b4b7
Alexey Zakhlestin <indeyets@gmail.com>
2011-03-22 15:18:11 +0300

    test for reddit calculation

--- tests/calculate.php
+++ tests/calculate.php
@@ -67,7 +67,16 @@ class com_meego_planet_tests_calculate extends midgardmvc_core_tests_testcase
         $score = com_meego_planet_calculate::hackernews('http://bergie.iki.fi/blog/introducing_the_midgard_create_user_interface/');
         $this->assertGreaterThanOrEqual(100, $score);
     }
-    
+
+    public function test_reddit()
+    {
+        $this->assert_urls('com_meego_planet_calculate::reddit');
+
+        // Test also with an URL we know has been posted
+        $score = com_meego_planet_calculate::reddit('http://www.reddit.com/');
+        $this->assertGreaterThanOrEqual(10, $score);
+    }
+
     public function test_age()
     {
         $penalty = com_meego_planet_calculate::age(new DateTime(), 1);
