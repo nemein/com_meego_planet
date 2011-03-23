8856ae4280c1693655529baa0b4f9b9c4b47cf45
Henri Bergius <henri.bergius@iki.fi>
2011-03-23 15:35:22 +0200

    Injection isn't needed here

--- injector.php
+++ injector.php
@@ -34,9 +34,6 @@ class com_meego_planet_injector
      */
     public function inject_template(midgardmvc_core_request $request)
     {
-        // Ensure our elements are available also for other components
-        $request->add_component_to_chain(midgardmvc_core::get_instance()->component->get('com_meego_planet'));
-
         // Replace the default MeeGo sidebar with our own
         $route = $request->get_route();
         $route->template_aliases['content-sidebar'] = 'cmp-show-sidebar';
